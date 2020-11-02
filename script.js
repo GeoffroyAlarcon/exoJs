c6e2772648fea1e81a173dfa997a2b1909957d34
GeoffroyALARCON <Votre@email.com>
2020-11-02 22:39:31 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let cl = console.log;
+let option;
+let verbe = document.getElementById("verbe");
+let resultat = document.getElementById("resultat");
+let selectPronom = document.getElementById("pronom");
+let tabPronom = ["je", "tu", "il/on/elle", "nous ", "vous", "ils/elles"];
+let tabConjugaison = ["e", "es", "e", "eons", "ez", "ent"]
+var indexConjugaison;
+function pronom() {
+
+    for (let x = 0; x < tabPronom.length; x++) {
+        option = document.createElement("button");
+        selectPronom.appendChild(option).innerHTML = tabPronom[x];
+        option.id = "btn" + x;
+        option.value = x;
+
+    }
+    let button = document.getElementsByTagName("button")
+
+    for (const elt of button) {
+        elt.addEventListener("click", function test() {
+            let string = "";
+            string = verbe.value
+
+            resultat.value = string.replace("er", tabConjugaison[elt.value]);
+
+        })
+    }
+
+   
+
+}
+pronom();
