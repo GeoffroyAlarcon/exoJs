904f49c187c25467a83fcc049d90217eaf1df350
GeoffroyAlarcon <geoffroy.alarcon@gmail.com>
2020-11-03 14:43:39 +0100

    first commit

--- script.js
+++ script.js
@@ -1,31 +1,8 @@
-let cl = console.log;
-let verbe = document.getElementById("verbe");
-let resultat = document.getElementById("resultat");
-let selectPronom = document.getElementById("pronom");
-let tabPronom = ["je", "tu", "il/on/elle", "nous ", "vous", "ils/elles"];
-let tabConjugaison = ["e", "es", "e", "eons", "ez", "ent"]
-function pronom() {
-
-    for (let x = 0; x < tabPronom.length; x++) {
-        let option = document.createElement("button");
-        selectPronom.appendChild(option).innerHTML = tabPronom[x];
-        option.id = "btn" + x;
-        option.value = x;
-
+var nom = document.getElementById('nom');
+nom.addEventListener('keypress', function (event) {
+    var pressedKey = event.key;
+    if (pressedKey < 'a' || pressedKey > 'z') {
+        event.preventDefault();
+        alert("Merci de tout écrire en minuscule");
     }
-    let button = document.getElementsByTagName("button")
-
-    for (const elt of button) {
-        elt.addEventListener("click", function test() {
-            let string = "";
-            string = verbe.value
-
-            resultat.value = string.replace("er", tabConjugaison[elt.value]);
-
-        })
-    }
-
-   
-
-}
-pronom();
+});
